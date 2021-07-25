c169d8eaa737f2643f15cc1a771003bda5f68abd
th-inu <krng0111@naver.com>
2021-07-25 18:51:48 +0900

    add notification

--- app/src/main/java/com/example/myappbykotlin_1/AlarmModeActivity.kt
+++ app/src/main/java/com/example/myappbykotlin_1/AlarmModeActivity.kt
@@ -1,10 +1,17 @@
 package com.example.myappbykotlin_1
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.EditText;
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.example.myappbykotlin_1.databinding.ActivityAlarmModeBinding
 import com.example.myappbykotlin_1.databinding.ActivityAlarmRecordBinding
 import com.example.myappbykotlin_1.databinding.ActivityMainBinding
@@ -27,6 +34,39 @@ class AlarmModeActivity : AppCompatActivity() {
         val binding=ActivityAlarmModeBinding.inflate(layoutInflater) //뷰 바인딩 사용 준비
         setContentView(binding.root) //화면 안의 버튼 사용 가능
 
+        switchPush.setOnCheckedChangeListener { CompoundButton, onSwitch ->
+            if (onSwitch) {
+                var NOTIFICATION_ID = 1001;
+
+                var builder = NotificationCompat.Builder( this, "myChannel")
+                    .setSmallIcon(R.drawable.back)
+                    .setContentTitle("Test Alarm")
+                    .setContentText("testing")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val name = "channel name";
+                    val descriptionText = "test Channel";
+                    val importance = NotificationManager.IMPORTANCE_DEFAULT;
+                    val channel = NotificationChannel("myChannel", name, importance).apply {
+                        description = descriptionText;
+                    }
+                    // Register the channel with the system
+                    val notificationManager: NotificationManager =
+                        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
+                    notificationManager.createNotificationChannel(channel);
+                }
+
+                //NotificationManagerCompat.from(this).notify(NOTIFICATION_ID, builder.build());
+
+                with(NotificationManagerCompat.from(this)) {
+                    // notificationId is a unique int for each notification that you must define
+                    notify(NOTIFICATION_ID, builder.build());
+                }
+            }
+        }
+
         binding.btnInput.setOnClickListener {
             val value: String = inputValue.text.toString();
 
@@ -39,19 +79,13 @@ class AlarmModeActivity : AppCompatActivity() {
                 } else { // 맞으면 AlarmRecord로 입력값을 보냄
                     val intent = Intent(this, AlarmRecord::class.java)
                     intent.putExtra("goalValue", value);
+
                     startActivity(intent);
                 }
             }
         }
 
-        // 버튼 클릭 시 화면 전환
-        //binding.btnBackPage.setOnClickListener{
-        //val nextIntent = Intent(this, AlarmRecord::class.java)
-        //startActivity(nextIntent)
-        //Log.d("Btn", "Alarm Record Btn is clicked! method = Log.d")
-    //} //binding 변수로 뷰에서 만든 버튼에 접근 가능
     }
-
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
