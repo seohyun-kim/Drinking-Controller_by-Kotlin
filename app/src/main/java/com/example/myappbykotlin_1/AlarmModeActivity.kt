ba3b4212a153331444a577c977add8b480f74319
th-inu <krng0111@naver.com>
2021-07-25 19:43:22 +0900

    modify little

--- app/src/main/java/com/example/myappbykotlin_1/AlarmModeActivity.kt
+++ app/src/main/java/com/example/myappbykotlin_1/AlarmModeActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myappbykotlin_1
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
@@ -8,13 +7,9 @@ import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.EditText;
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.myappbykotlin_1.databinding.ActivityAlarmModeBinding
-import com.example.myappbykotlin_1.databinding.ActivityAlarmRecordBinding
-import com.example.myappbykotlin_1.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_alarm_mode.*
 
 class AlarmModeActivity : AppCompatActivity() {
@@ -34,38 +29,19 @@ class AlarmModeActivity : AppCompatActivity() {
         val binding=ActivityAlarmModeBinding.inflate(layoutInflater) //뷰 바인딩 사용 준비
         setContentView(binding.root) //화면 안의 버튼 사용 가능
 
+        // notification 설정
+        var NOTIFICATION_ID = 1001;
+        var channelID = "myChannel";
+        var channelName = "channel name";
+        var channelDiscription = "test Channel"
+        createNotificationChannel(channelID, channelName, channelDiscription)
+
         switchPush.setOnCheckedChangeListener { CompoundButton, onSwitch ->
             if (onSwitch) {
-                var NOTIFICATION_ID = 1001;
 
-                var builder = NotificationCompat.Builder( this, "myChannel")
-                    .setSmallIcon(R.drawable.back)
-                    .setContentTitle("Test Alarm")
-                    .setContentText("testing")
-                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    val name = "channel name";
-                    val descriptionText = "test Channel";
-                    val importance = NotificationManager.IMPORTANCE_DEFAULT;
-                    val channel = NotificationChannel("myChannel", name, importance).apply {
-                        description = descriptionText;
-                    }
-                    // Register the channel with the system
-                    val notificationManager: NotificationManager =
-                        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager;
-                    notificationManager.createNotificationChannel(channel);
-                }
-
-                //NotificationManagerCompat.from(this).notify(NOTIFICATION_ID, builder.build());
-
-                with(NotificationManagerCompat.from(this)) {
-                    // notificationId is a unique int for each notification that you must define
-                    notify(NOTIFICATION_ID, builder.build());
-                }
             }
         }
+
 
         binding.btnInput.setOnClickListener {
             val value: String = inputValue.text.toString();
@@ -80,12 +56,43 @@ class AlarmModeActivity : AppCompatActivity() {
                     val intent = Intent(this, AlarmRecord::class.java)
                     intent.putExtra("goalValue", value);
 
+                    // notify
+                    var builder = NotificationCompat.Builder( this, channelID)
+                        .setSmallIcon(R.drawable.mainpage_beer)
+                        .setContentTitle("Input Value")
+                        .setContentText(value)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+                    with(NotificationManagerCompat.from(this)) {
+                        notify(NOTIFICATION_ID, builder.build());
+                    }
                     startActivity(intent);
                 }
             }
         }
 
     }
+    private fun createNotificationChannel(
+        channelID: String,
+        channelName: String,
+        channelDiscription: String
+    ) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = channelName
+            val descriptionText = channelDiscription
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(channelID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
