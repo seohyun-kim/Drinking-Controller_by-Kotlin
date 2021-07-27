fa58148af1818281a10fa23835ace69095870d0f
th-inu <krng0111@naver.com>
2021-07-27 23:15:03 +0900

    test commit don't use

--- app/src/main/java/com/example/myappbykotlin_1/MainActivity.kt
+++ app/src/main/java/com/example/myappbykotlin_1/MainActivity.kt
@@ -1,53 +1,414 @@
 package com.example.myappbykotlin_1
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+import android.bluetooth.BluetoothGattServer
+import android.bluetooth.BluetoothGattServerCallback
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.os.ParcelUuid
+import android.text.format.DateFormat
 import android.util.Log
-import android.widget.Toast
+import android.view.WindowManager
+import android.widget.TextView
 
+import java.util.Arrays
+import java.util.Date
 
-import com.example.myappbykotlin_1.databinding.ActivityMainBinding //안드로이드가 자동으로 변환함
+private const val TAG = "GattServerActivity"
 
-class MainActivity : AppCompatActivity() {
-    private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
-    override fun onResume() {
-        super.onResume()
+class GattServerActivity : Activity() {
 
-        packageManager.takeIf { it.missingSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE) }?.also {
-            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show()
-            finish()
+    /* Local UI */
+    private lateinit var localTimeView: TextView
+    /* Bluetooth API */
+    private lateinit var bluetoothManager: BluetoothManager
+    private var bluetoothGattServer: BluetoothGattServer? = null
+    /* Collection of notification subscribers */
+    private val registeredDevices = mutableSetOf<BluetoothDevice>()
+
+    /**
+     * Listens for system time changes and triggers a notification to
+     * Bluetooth subscribers.
+     */
+    private val timeReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val adjustReason = when (intent.action) {
+                Intent.ACTION_TIME_CHANGED -> TimeProfile.ADJUST_MANUAL
+                Intent.ACTION_TIMEZONE_CHANGED -> TimeProfile.ADJUST_TIMEZONE
+                Intent.ACTION_TIME_TICK -> TimeProfile.ADJUST_NONE
+                else -> TimeProfile.ADJUST_NONE
+            }
+            val now = System.currentTimeMillis()
+            notifyRegisteredDevices(now, adjustReason)
+            updateLocalUi(now)
         }
     }
 
+    /**
+     * Listens for Bluetooth adapter events to enable/disable
+     * advertising and server functionality.
+     */
+    private val bluetoothReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF)
 
+            when (state) {
+                BluetoothAdapter.STATE_ON -> {
+                    startAdvertising()
+                    startServer()
+                }
+                BluetoothAdapter.STATE_OFF -> {
+                    stopServer()
+                    stopAdvertising()
+                }
+            }
+        }
+    }
 
+    /**
+     * Callback to receive information about the advertisement process.
+     */
+    private val advertiseCallback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        object : AdvertiseCallback() {
+            override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
+                Log.i(TAG, "LE Advertise Started.")
+            }
+
+            override fun onStartFailure(errorCode: Int) {
+                Log.w(TAG, "LE Advertise Failed: $errorCode")
+            }
+        }
+    } else {
+        TODO("VERSION.SDK_INT < LOLLIPOP")
+    }
+
+    /**
+     * Callback to handle incoming requests to the GATT server.
+     * All read/write requests for characteristics and descriptors are handled here.
+     */
+    private val gattServerCallback = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+        object : BluetoothGattServerCallback() {
+
+            override fun onConnectionStateChange(device: BluetoothDevice, status: Int, newState: Int) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    Log.i(TAG, "BluetoothDevice CONNECTED: $device")
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    Log.i(TAG, "BluetoothDevice DISCONNECTED: $device")
+                    //Remove device from any active subscriptions
+                    registeredDevices.remove(device)
+                }
+            }
+
+            override fun onCharacteristicReadRequest(device: BluetoothDevice, requestId: Int, offset: Int,
+                                                     characteristic: BluetoothGattCharacteristic) {
+                val now = System.currentTimeMillis()
+                when {
+                    TimeProfile.CURRENT_TIME == characteristic.uuid -> {
+                        Log.i(TAG, "Read CurrentTime")
+                        bluetoothGattServer?.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_SUCCESS,
+                            0,
+                            TimeProfile.getExactTime(now, TimeProfile.ADJUST_NONE))
+                    }
+                    TimeProfile.LOCAL_TIME_INFO == characteristic.uuid -> {
+                        Log.i(TAG, "Read LocalTimeInfo")
+                        bluetoothGattServer?.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_SUCCESS,
+                            0,
+                            TimeProfile.getLocalTimeInfo(now))
+                    }
+                    else -> {
+                        // Invalid characteristic
+                        Log.w(TAG, "Invalid Characteristic Read: " + characteristic.uuid)
+                        bluetoothGattServer?.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_FAILURE,
+                            0,
+                            null)
+                    }
+                }
+            }
+
+            override fun onDescriptorReadRequest(device: BluetoothDevice, requestId: Int, offset: Int,
+                                                 descriptor: BluetoothGattDescriptor) {
+                if (TimeProfile.CLIENT_CONFIG == descriptor.uuid) {
+                    Log.d(TAG, "Config descriptor read")
+                    val returnValue = if (registeredDevices.contains(device)) {
+                        BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+                    } else {
+                        BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE
+                    }
+                    bluetoothGattServer?.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_SUCCESS,
+                        0,
+                        returnValue)
+                } else {
+                    Log.w(TAG, "Unknown descriptor read request")
+                    bluetoothGattServer?.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0, null)
+                }
+            }
+
+            override fun onDescriptorWriteRequest(device: BluetoothDevice, requestId: Int,
+                                                  descriptor: BluetoothGattDescriptor,
+                                                  preparedWrite: Boolean, responseNeeded: Boolean,
+                                                  offset: Int, value: ByteArray) {
+                if (TimeProfile.CLIENT_CONFIG == descriptor.uuid) {
+                    if (Arrays.equals(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE, value)) {
+                        Log.d(TAG, "Subscribe device to notifications: $device")
+                        registeredDevices.add(device)
+                    } else if (Arrays.equals(BluetoothGattDescriptor.DISABLE_NOTIFICATION_VALUE, value)) {
+                        Log.d(TAG, "Unsubscribe device from notifications: $device")
+                        registeredDevices.remove(device)
+                    }
+
+                    if (responseNeeded) {
+                        bluetoothGattServer?.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_SUCCESS,
+                            0, null)
+                    }
+                } else {
+                    Log.w(TAG, "Unknown descriptor write request")
+                    if (responseNeeded) {
+                        bluetoothGattServer?.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_FAILURE,
+                            0, null)
+                    }
+                }
+            }
+        }
+    } else {
+        TODO("VERSION.SDK_INT < JELLY_BEAN_MR2")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // res/layout 디렉토리에 있는 activity_main.xml 파일을 사용한다
+        setContentView(R.layout.activity_server)
 
-        //뷰 바인딩
-        val binding= ActivityMainBinding.inflate(layoutInflater) //뷰 바인딩 사용 준비
-        setContentView(binding.root) //화면 안의 버튼 사용 가능
+        localTimeView = findViewById(R.id.text_time)
 
-        //측정모드 버튼 클릭 시 화면 전환
-        binding.MessureModeBtn.setOnClickListener{
-            val nextIntent = Intent(this, MessureModeActivity::class.java)
-            startActivity(nextIntent)
-            Log.d("MainBtn", "Messure Mode Btn is clicked! method = Log.d")
-        } //binding 변수로 뷰에서 만든 버튼에 접근 가능
+        // Devices with a display should not go to sleep
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
-        //측정모드 버튼 클릭 시 화면 전환
-        binding.AlarmModeBtn.setOnClickListener{
-            val nextIntent = Intent(this, AlarmModeActivity::class.java)
-            startActivity(nextIntent)
-            Log.d("MainBtn", "Alarm Mode Btn is clicked! method = Log.d")
-        } //binding 변수로 뷰에서 만든 버튼에 접근 가능
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        }
+        val bluetoothAdapter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            bluetoothManager.adapter
+        } else {
+            TODO("VERSION.SDK_INT < JELLY_BEAN_MR2")
+        }
+        // We can't continue without proper Bluetooth support
+        if (!checkBluetoothSupport(bluetoothAdapter)) {
+            finish()
+        }
 
+        // Register for system Bluetooth events
+        val filter = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
+        registerReceiver(bluetoothReceiver, filter)
+        if (!bluetoothAdapter.isEnabled) {
+            Log.d(TAG, "Bluetooth is currently disabled...enabling")
+            bluetoothAdapter.enable()
+        } else {
+            Log.d(TAG, "Bluetooth enabled...starting services")
+            startAdvertising()
+            startServer()
+        }
+    }
 
+    override fun onStart() {
+        super.onStart()
+        // Register for system clock events
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_TIME_TICK)
+            addAction(Intent.ACTION_TIME_CHANGED)
+            addAction(Intent.ACTION_TIMEZONE_CHANGED)
+        }
 
+        registerReceiver(timeReceiver, filter)
+    }
 
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(timeReceiver)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val bluetoothAdapter = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            bluetoothManager.adapter
+        } else {
+            TODO("VERSION.SDK_INT < JELLY_BEAN_MR2")
+        }
+        if (bluetoothAdapter.isEnabled) {
+            stopServer()
+            stopAdvertising()
+        }
+
+        unregisterReceiver(bluetoothReceiver)
+    }
+
+    /**
+     * Verify the level of Bluetooth support provided by the hardware.
+     * @param bluetoothAdapter System [BluetoothAdapter].
+     * @return true if Bluetooth is properly supported, false otherwise.
+     */
+    private fun checkBluetoothSupport(bluetoothAdapter: BluetoothAdapter?): Boolean {
+
+        if (bluetoothAdapter == null) {
+            Log.w(TAG, "Bluetooth is not supported")
+            return false
+        }
+
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Log.w(TAG, "Bluetooth LE is not supported")
+            return false
+        }
+
+        return true
+    }
+
+    /**
+     * Begin advertising over Bluetooth that this device is connectable
+     * and supports the Current Time Service.
+     */
+    private fun startAdvertising() {
+        val bluetoothLeAdvertiser: BluetoothLeAdvertiser? =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    bluetoothManager.adapter.bluetoothLeAdvertiser
+                } else {
+                    TODO("VERSION.SDK_INT < LOLLIPOP")
+                }
+            } else {
+                TODO("VERSION.SDK_INT < JELLY_BEAN_MR2")
+            }
+
+        bluetoothLeAdvertiser?.let {
+            val settings = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                AdvertiseSettings.Builder()
+                    .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                    .setConnectable(true)
+                    .setTimeout(0)
+                    .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                    .build()
+            } else {
+                TODO("VERSION.SDK_INT < LOLLIPOP")
+            }
+
+            val data = AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .setIncludeTxPowerLevel(false)
+                .addServiceUuid(ParcelUuid(TimeProfile.TIME_SERVICE))
+                .build()
+
+            it.startAdvertising(settings, data, advertiseCallback)
+        } ?: Log.w(TAG, "Failed to create advertiser")
+    }
+
+    /**
+     * Stop Bluetooth advertisements.
+     */
+    private fun stopAdvertising() {
+        val bluetoothLeAdvertiser: BluetoothLeAdvertiser? =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    bluetoothManager.adapter.bluetoothLeAdvertiser
+                } else {
+                    TODO("VERSION.SDK_INT < LOLLIPOP")
+                }
+            } else {
+                TODO("VERSION.SDK_INT < JELLY_BEAN_MR2")
+            }
+        bluetoothLeAdvertiser?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                it.stopAdvertising(advertiseCallback)
+            }
+        } ?: Log.w(TAG, "Failed to create advertiser")
+    }
+
+    /**
+     * Initialize the GATT server instance with the services/characteristics
+     * from the Time Profile.
+     */
+    private fun startServer() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            bluetoothGattServer = bluetoothManager.openGattServer(this, gattServerCallback)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            bluetoothGattServer?.addService(TimeProfile.createTimeService())
+                ?: Log.w(TAG, "Unable to create GATT server")
+        }
+
+        // Initialize the local UI
+        updateLocalUi(System.currentTimeMillis())
+    }
+
+    /**
+     * Shut down the GATT server.
+     */
+    private fun stopServer() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            bluetoothGattServer?.close()
+        }
+    }
+
+    /**
+     * Send a time service notification to any devices that are subscribed
+     * to the characteristic.
+     */
+    private fun notifyRegisteredDevices(timestamp: Long, adjustReason: Byte) {
+        if (registeredDevices.isEmpty()) {
+            Log.i(TAG, "No subscribers registered")
+            return
+        }
+        val exactTime = TimeProfile.getExactTime(timestamp, adjustReason)
+
+        Log.i(TAG, "Sending update to ${registeredDevices.size} subscribers")
+        for (device in registeredDevices) {
+            val timeCharacteristic = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                bluetoothGattServer
+                    ?.getService(TimeProfile.TIME_SERVICE)
+                    ?.getCharacteristic(TimeProfile.CURRENT_TIME)
+            } else {
+                TODO("VERSION.SDK_INT < JELLY_BEAN_MR2")
+            }
+            timeCharacteristic?.value = exactTime
+            bluetoothGattServer?.notifyCharacteristicChanged(device, timeCharacteristic, false)
+        }
+    }
+
+    /**
+     * Update graphical UI on devices that support it with the current time.
+     */
+    private fun updateLocalUi(timestamp: Long) {
+        val date = Date(timestamp)
+        val displayDate = DateFormat.getMediumDateFormat(this).format(date)
+        val displayTime = DateFormat.getTimeFormat(this).format(date)
+        localTimeView.text = "$displayDate\n$displayTime"
     }
 }
