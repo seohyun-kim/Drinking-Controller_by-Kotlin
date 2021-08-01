386600fc9230a0bbef7356c246480b6c2c6281fd
seokahi <skh9797@naver.com>
2021-08-01 15:24:40 +0900

    modified ui

--- app/src/main/java/com/example/myappbykotlin_1/MessureRecord.kt
+++ app/src/main/java/com/example/myappbykotlin_1/MessureRecord.kt
@@ -40,7 +40,7 @@ class MessureRecord: AppCompatActivity() {
         //뷰 바인딩
         val binding=ActivityMessureRecordBinding.inflate(layoutInflater) //뷰 바인딩 사용 준비
         setContentView(binding.root) //화면 안의 버튼 사용 가능
-        var drinkcapacity :Float = 264F;
+        var drinkcapacity :Float = 150F;
         var ml_drinkcapacity :Float = drinkcapacity/50;
         binding.drinkcapacitytext.text= drinkcapacity.toString()+" ml " + ml_drinkcapacity.toString()+" 잔 ";
         binding.homebackbtn.setOnClickListener{
